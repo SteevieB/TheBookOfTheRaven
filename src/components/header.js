3de2fb125165abbe10fce0dafe1c56fba00dfe74
Stefan Brandl <steevie.brandl@gmail.com>
2021-05-28 21:00:12 +0200

    design update

--- src/components/header.js
+++ src/components/header.js
@@ -11,10 +11,10 @@ const Header = ({ siteTitle, subTitle }) => (
       </div>
       <div className="z-1 h-44 sm:h-48 md:h-48 -mt-44 sm:-mt-48 relative">
         <div className="text-center p-12">
-          <h1 className="text-prim-light drop-shadow text-5xl md:text-6xl">
+          <h1 className="text-prim-light drop-shadow font-noto text-5xl md:text-6xl">
             <Link to="/">{siteTitle}</Link>
           </h1>
-          <h2 className="text-prim-light text-2xl font-roboto">{subTitle}</h2>
+          <h2 className="text-prim-light text-2xl font-noto">{subTitle}</h2>
         </div>
       </div>
       <div className="relative z-1">
